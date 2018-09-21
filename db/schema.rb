f013391f6c6e70e3aba47ec2d1678ac50666eb96
ottagit <ottacc66@gmail.com>
2018-09-21 20:49:01 +0300

    genereate trade model
    change root route to trade index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180921164226) do
+ActiveRecord::Schema.define(version: 20180921173741) do
+
+  create_table "trades", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_trades_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
